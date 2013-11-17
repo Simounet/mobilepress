ce851dc5b9fd61b9705723f897a4c61f868bb3ac
Simounet <contact@simounet.net>
2013-11-17 12:31:29 +0100

    Setting the right input types for mobile keyboard enhancement

--- themes/default/postcomment.php
+++ themes/default/postcomment.php
@@ -24,10 +24,10 @@
 							<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="14" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
 							<label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
 
-							<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="14" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+							<p><input type="email" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="14" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
 							<label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
 
-							<p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="14" tabindex="3" />
+							<p><input type="url" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="14" tabindex="3" />
 							<label for="url"><small>Website</small></label></p>
 
 							<?php endif; ?>
